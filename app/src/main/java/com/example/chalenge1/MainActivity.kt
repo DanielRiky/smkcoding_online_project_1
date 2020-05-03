7911394cd6563cc6c27c98421a9906ad353ad391
danielriky <danielriky30@gmail.com>
2020-05-03 12:02:11 +0700

    project init

---
+++ app/src/main/java/com/example/chalenge1/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.chalenge1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+//    external fun stringFromJNI(): String
+//
+//    companion object {
+//
+//        // Used to load the 'native-lib' library on application startup.
+//        init {
+//            System.loadLibrary("native-lib")
+//        }
+//    }
+}
